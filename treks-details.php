bf5a7ea2146fd5d0e7c10b6d1831fc0c5b09251d
Anit kumar jha <73160412+Anit-jha88@users.noreply.github.com>
2026-08-17 12:29:13 +0530

    Fix formatting in treks-details

--- treks-details.php
+++ treks-details.php
@@ -7,7 +7,7 @@ $currentPage = 'treksPage';
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" /> 
-
+ 
         <!-- Start Header Section -->
             <?php include('./components/header.php') ?>
         <!-- End Header Section -->
