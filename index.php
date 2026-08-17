75c9e9f5f619e5a3197e71127fed051da9207457
Anit kumar jha <73160412+Anit-jha88@users.noreply.github.com>
2026-08-17 12:30:08 +0530

    Fix formatting

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include('admin/config.php');
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
+ 
         <meta name="keywords" content="" />	
 		<meta name="description" content="Buddy Hikers" />
 	    <meta name="author" content="MKS Technosoft "> 
