08e93fec0b0c63123e11efcca6323c2483a2a683
Anit kumar jha <73160412+Anit-jha88@users.noreply.github.com>
2026-08-17 12:28:13 +0530

    Update treks-details.php
    changea

--- treks-details.php
+++ treks-details.php
@@ -3,7 +3,7 @@ include('admin/config.php');
 $currentPage = 'treksPage';
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"> 
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
